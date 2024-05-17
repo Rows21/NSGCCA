fix normalization problems

SNGCCA/Simulation2_Col.py
@@ -1,81 +1,63 @@
-from SNGCCA.main import Solver
 import torch
-from synth_data import create_synthData_new, create_synthData_multi
-from validation_method import FS_MCC
-
-import os
 import numpy as np
-import pandas as pd
+from validation_method import eval
 
-if __name__ == '__main__':
-    # Slurm Object note
-    import sys
-    #rs = os.environ.get('SLURM_JOB_ID')
-    #torch.manual_seed(rs)  # random seed
+num = 20
+sample = 100
+tol = 100
+root = 'D:/GitHub/SNGCCA/SNGCCA/Simulation/'
+dir_sce = 'Linear/' + str(sample) + '_' + str(tol) + '_' + str(num) + '/'
+print(dir_sce)
+path = root + dir_sce
+u1 = np.genfromtxt(path + 'u1.csv', delimiter=',')
+u2 = np.genfromtxt(path + 'u2.csv', delimiter=',')
+u3 = np.genfromtxt(path + 'u3.csv', delimiter=',')
+Label = torch.cat([torch.ones(num, dtype=torch.bool), torch.zeros(tol-num, dtype=torch.bool)])
 
-    # Hyper Params Section
-    device = 'cuda' if torch.cuda.is_available() else 'cpu'
-    print("Using", torch.cuda.device_count(), "GPUs")
+FS = []
+MCC = []
+PRE = []
+REC = []
+SPE = []
+for i in range(100):
+    u = [torch.tensor(u1[i]),torch.tensor(u2[i]),torch.tensor(u3[i])]
+    spe, pre, rec, acc, f1, mcc = eval(u, Label)
+    SPE.append(spe)
+    PRE.append(pre)
+    REC.append(rec)
+    FS.append(f1)
+    MCC.append(mcc)
 
-    Solver = Solver(device)
+print("Specificity:",np.mean(SPE),np.std(SPE))
+print("Precision:",np.mean(PRE),np.std(PRE))
+print("Recall:",np.mean(REC),np.std(REC))
+print("FS:",np.mean(FS),np.std(FS))
+print("MCC:",np.mean(MCC),np.std(MCC))
 
-    for scenario in [3]:
-    ## Scenario
-        ## Evaluation params
-        ACC_list = []
-        FS_list = []
-        MCC_list = []
+dir_sce = 'Nonlinear/' + str(sample) + '_' + str(tol) + '_' + str(num) + '/'
+print(dir_sce)
+path = root + dir_sce
+u1 = np.genfromtxt(path + 'u1.csv', delimiter=',')
+u2 = np.genfromtxt(path + 'u2.csv', delimiter=',')
+u3 = np.genfromtxt(path + 'u3.csv', delimiter=',')
+Label = torch.cat([torch.ones(num, dtype=torch.bool), torch.zeros(tol-num, dtype=torch.bool)])
 
-        for i in range(4, 11, 2):
-            print("Scenario=",scenario,"Column=",i)
-            FS = []
-            MCC = []
-            ACC = []
-            N = 400
-            views = create_synthData_multi(i=i, data_type=scenario, N=400, p=20, q=20, r=20)
-            print(f'input views shape :')
-            for j, view in enumerate(views):
-                print(f'view_{j} :  {view.shape}')
-                view = view.to("cpu")
+FS = []
+MCC = []
+PRE = []
+REC = []
+SPE = []
+for i in range(100):
+    u = [torch.tensor(u1[i]),torch.tensor(u2[i]),torch.tensor(u3[i])]
+    spe, pre, rec, acc, f1, mcc = eval(u, Label)
+    SPE.append(spe)
+    PRE.append(pre)
+    REC.append(rec)
+    FS.append(f1)
+    MCC.append(mcc)
 
-            ## train hyper
-            b0, obj = Solver.tune_hyper(x_list=views, set_params=6, max_params=50,loss="SGD", iters=100)
-            print(b0)
-
-            print("SNGCCA Started!")
-            for rep in range(100):
-                if (rep + 1) % 100 == 0:
-                    print("REP=", rep + 1)
-
-                ## fit results
-                u = Solver._get_outputs(views, 1e-7, 300, (b0,b0,b0))
-
-                Label = torch.cat([torch.ones(i, dtype=torch.bool), torch.zeros(20-i, dtype=torch.bool)])
-                acc, f1, mcc = FS_MCC(u, Label)
-                ACC.append(acc)
-                FS.append(f1)
-                MCC.append(mcc)
-
-            df = pd.DataFrame({'Accuracy': ACC,
-                               'F-Score': FS,
-                               'MCC': MCC})
-
-            dir_path = "./Simulation"
-            if not os.path.exists(dir_path):
-                os.mkdir(dir_path)
-
-            df.to_csv('./Simulation/Scenario' + str(scenario) + 'Colunm' + str(i) + '.csv')
-            macc = np.mean(ACC)
-            sdacc = np.std(ACC)
-            print(macc, sdacc)
-            ACC_list.append([macc, sdacc])
-
-            mf = np.mean(FS)
-            sdf = np.std(FS)
-            print(mf, sdf)
-            FS_list.append([mf, sdf])
-
-            mmcc = np.mean(MCC)
-            sdmcc = np.std(MCC)
-            print(mmcc, sdmcc)
-            MCC_list.append([mmcc, sdmcc])
+print("Specificity:",np.mean(SPE),np.std(SPE))
+print("Precision:",np.mean(PRE),np.std(PRE))
+print("Recall:",np.mean(REC),np.std(REC))
+print("FS:",np.mean(FS),np.std(FS))
+print("MCC:",np.mean(MCC),np.std(MCC))

SNGCCA/main.py
@@ -174,30 +174,29 @@ if __name__ == '__main__':
     PRE = []
     REC = []
 
-    while rep != 100:
-        views = create_synthData_new(5,N, mode=2, F=200)
+    for rep in range(100):
+        print("REP=",rep)
+        views = create_synthData_new(5,N, mode=1, F=100)
         solver = Solver(device)
-        b = [0.015,0.02,0.02]
-        try:
-            u = solver.SNGCCA.fit_admm2(views, lamb=b,logging=0)  
-        except:
-            continue
-        
-        Label = torch.cat([torch.ones(5, dtype=torch.bool), torch.zeros(195, dtype=torch.bool)])
-        spe, pre, rec, acc, f1, mcc = eval(u, Label)
-        if mcc > 0.60:
-            rep += 1
-            print("REP=",rep)
-            PRE.append(pre)
-            REC.append(rec)
-            ACC.append(acc)
-            FS.append(f1)
-            MCC.append(mcc)
-            u1.append(u[0])
-            u2.append(u[1])
-            u3.append(u[2])
+        b = [0.004,0.004,0.004]
+        b = [i *1 for i in b]
+        u = solver.SNGCCA.fit_admm2(views, lamb=b,logging=0)
 
-    merged_array = merged_array = np.empty((100, 80))
+        Label = torch.cat([torch.ones(5, dtype=torch.bool), torch.zeros(95, dtype=torch.bool)])
+        spe, pre, rec, acc, f1, mcc = eval(u, Label)
+        print(mcc)
+
+        
+        PRE.append(pre)
+        REC.append(rec)
+        ACC.append(acc)
+        FS.append(f1)
+        MCC.append(mcc)
+        u1.append(u[0])
+        u2.append(u[1])
+        u3.append(u[2])
+
+    merged_array = merged_array = np.empty((100, 100))
 
     for i, arr in enumerate(u1):
         merged_array[i] = u1[i].numpy().flatten()

SNGCCA/sngcca.py
@@ -89,8 +89,8 @@ class SNGCCA():
         self.L_list = []
         self.Ly_list = []
         for i, view in enumerate(views):
-            n, p = view.shape
 
+            n, p = view.shape
             # set sqcovx
             covx = torch.cov(view.T)
             self.covx_list.append(covx)
@@ -112,7 +112,7 @@ class SNGCCA():
             u = torch.ones((p, 1))
             u = torch.sqrt(u / torch.norm(u, p=2))
             self.u_list[i] = u
-            u = u / torch.sqrt(u.t() @ sqcovx @ u)
+            u = u * torch.sqrt(view.shape[0] / (u.t() @ view.t() @ view @ u)) 
             initPi = u @ u.t()
             initPi = torch.zeros((p,p))
             Pi = initPi#/torch.norm(initPi)
@@ -123,18 +123,12 @@ class SNGCCA():
             Kx = sx + sx.T - 2 * view @ Pi @ view.T
             Kx = torch.exp(-Kx / 2)
             self.K_list.append(Kx)
-
             self.y_lab.append(view @ u)
-
-            # construct Lipchitz Constant L
-            # product kernel
-            #y = np.genfromtxt('ydata.csv', delimiter=',')
-            #y = torch.tensor(y).reshape(-1,1)
         
         for i, view in enumerate(views):
             n, p = view.shape
-            y = sum([self.y_lab[j] for j in range(len(views))])/(len(views)) # if j != i
-            y = y / torch.sqrt(torch.sum(y ** 2))
+            y = sum([self.y_lab[j] for j in range(len(views)) if j != i])#/(len(views)) # if j != i
+            #y = y / torch.sqrt(torch.sum(y ** 2))
             sigmaY2 = torch.var(y)
             sy = torch.diag(y @ y.T).reshape(-1, 1)
 
@@ -157,42 +151,23 @@ class SNGCCA():
 
         print(self.L_list)
         #lamb = [i/100 * 1.5 for i in L_list]
-        outer_maxiter = 1000
+        outer_maxiter = 2000
         outer_tol = 1e-5
         inner_maxiter = 500
         inner_tol = 1e-3
         outer_error = 1
         diff_list = [999] * n_views
-
-        diff_log = [[999], [999], [999]]
-        norms = [None] * n_views
-        diff_L = self.L_list
-        out_idx = []
-        criterion = 1e-3/3
+        criterion = 1e-3
 
         progress_bar = tqdm(total=outer_maxiter, ncols=200)
         
         for outer_iter in range(outer_maxiter):
-            #if outer_error < outer_tol:
-            #    return Pi_list
-            Pi_list = self.Pi_list
-            H_list = self.H_list
-            Gamma_list = self.Gamma_list
-
-            L_list = self.L_list
-            Ly_list = self.Ly_list
-
             for i, view in enumerate(views):
-                #print(sum(diff_log[i][-1:-11:-1]))                    
-                #Ly_grad = [self.Ly_list[j] for j in range(len(views)) if j != i]
-                #Ly_grad = sum(Ly_grad)
+
                 Ly_grad = self.Ly_list[i]
-                    
                 #L_grad = [self.L_list[j] for j in range(len(views)) if j != i]
                 L_grad = self.L_list[i]#sum(L_grad)/len(L_grad)
-
                 u_pre = self.u_list[i]
-                    
                 Coeft = self.K_list[i] * Ly_grad /2
 
                 n = Coeft.shape[0]
@@ -204,7 +179,7 @@ class SNGCCA():
 
                 L = L_grad
                 a = Pi - dF / L
-                Pi_pre = Pi
+                #Pi_pre = Pi
                 inner_error = 1
                 inner_iter = 0
 
@@ -218,8 +193,6 @@ class SNGCCA():
                 while (inner_iter <= inner_maxiter) & (inner_error > inner_tol):
                     #print(inner_iter)
                     temp = Pi-(rho/tau) * covx @ Pi @ covx + (rho/tau) * sqcovx @ (H-Gamma) @ sqcovx
-                    #if (sum(sum(temp)) == torch.tensor(0)).item():
-                    #    break
                     temp = tau/(tau+L)*temp+L/(tau+L) * a
                     
                     Pi = torch.max((torch.abs(temp)- lamb[i]/(L + tau)), torch.zeros(temp.size())) * torch.sign(temp)                                
@@ -247,28 +220,23 @@ class SNGCCA():
                     Pi = torch.tensor(np.real(Pi))
 
                 outer_error = torch.norm(torch.abs(u_new-self.u_list[i]))
-                outer_error_P = torch.norm(torch.abs(Pi-Pi_pre))
-                #print(outer_error_P)
-                #print(u_new-self.u_list[i])
-                
-                #print(torch.norm(Pi))
-                #diff_list[i] = outer_error
-                #diff_log[i].append(outer_error)
-                u_new = u_new /  torch.sqrt(torch.sum(u_new ** 2))
+                #u_new = u_new * torch.sqrt(view.shape[0] / (u_new.t() @ view.t() @ view @ u_new)) 
 
                 if (outer_error > outer_tol).item() or (sum(abs(self.u_list[i]) - abs(u_pre)).numpy()[0] == 0):
 
                     self.u_list[i] = u_new
                     #print(torch.norm(u_new))
-                    Pi_list[i] = Pi / torch.norm(Pi)
-                    H_list[i] = H
-                    Gamma_list[i] = Gamma
+                    self.Pi_list[i] = Pi #/ torch.norm(Pi)
+                    self.H_list[i] = H
+                    self.Gamma_list[i] = Gamma
                             
                     # normalize
                     # new L
                     self.y_lab[i] = view @ u_new
-                    y = sum([self.y_lab[j] for j in range(len(views)) if j != i])/(len(views)-1)
-                    y = y / torch.sqrt(torch.sum(y ** 2))
+                    y_list = [self.y_lab[j] for j in range(len(views))]# if j != i
+                    y = torch.cat(y_list, dim=1)
+                    y = y / torch.std(y)
+                    #y = y / torch.sqrt(torch.sum(y ** 2))
                     #y = view @ u_new
                     sigmaY2 = torch.var(y)
 
@@ -279,16 +247,16 @@ class SNGCCA():
                     Ly = sy + sy.T - 2 * (y @ y.T)
                     Ly = torch.exp(-Ly / (2 * sigmaY2))
                     Ly = self.centre_kernel(Ly)
-                    Ly_list[i] = Ly 
+                    self.Ly_list[i] = Ly 
 
                     # new Lipchitz Constant L
                     Coef = - Ly * (Ly < 0) / 4
                     Coef = 2 * (torch.diag(torch.squeeze(torch.sum(Coef, dim=1, keepdim=True))) - Coef) / (n ** 2)
                     Coef = view.T @ Coef @ view
                     L, _ = torch.linalg.eigh((Coef + Coef.t()) / 2)
-                    L_list[i] = L[-1]
+                    self.L_list[i] = L[-1]
                         
-                    K0 = view @ Pi_list[i] @ view.T
+                    K0 = view @ self.Pi_list[i] @ view.T
                     #K0 = K0 / torch.norm(K0)
                     sx = torch.diag(K0).reshape(-1,1)
                     Kx_new = sx + sx.T - 2 * K0
@@ -299,12 +267,6 @@ class SNGCCA():
                 if abs(diff_list[i].numpy()[0]) <= 1e-10:
                     #print(self.u_list[i])
                     self.u_list[i] = u_pre
-            
-            self.Pi_list = Pi_list
-            self.H_list = H_list
-            self.Gamma_list = Gamma_list
-            self.L_list = L_list
-            self.Ly_list = Ly_list
 
             F_trial = - sum([lamb[i] * torch.norm(self.Pi_list[i], p=1) for i in range(len(self.Pi_list))])
             for items in itertools.combinations(range(len(self.K_list)), 2):
@@ -313,13 +275,11 @@ class SNGCCA():
             loss = '{:.4g}'.format(sum([abs(i.item()) for i in diff_list]))
             if logging == 1:
                 
-                print('outer_iter=', outer_iter, 'loss=', sum(diff_list), "diff_tol=",L_list, "diff_list=", diff_list, 'obj=', F_trial)
+                print('outer_iter=', outer_iter, 'loss=', sum(diff_list), "diff_tol=",self.L_list, "diff_list=", diff_list, 'obj=', F_trial)
             elif logging == 0:
                 #print(f"outer_iter=: {outer_iter}, Loss: {sum(diff_list)}, diff_tol: {L_list}, diff_list: {diff_list}, obj: {F_trial}")
-                progress_bar.set_description(f"outer_iter=: {outer_iter},obj: {'{:.4g}'.format(F_trial)}, Loss: {loss}, diff_tol: {L_list}, diff_list: {diff_list}")
+                progress_bar.set_description(f"outer_iter=: {outer_iter},obj: {'{:.4g}'.format(F_trial)}, Loss: {loss}, diff_tol: {self.L_list}, diff_list: {diff_list}")
                 #progress_bar.set_postfix({'Iter': outer_iter+1})
             if sum([abs(i.item()) for i in diff_list])/len(diff_list) < criterion:
                 return self.u_list
         return self.u_list
-
-

SNGCCA/synth_data.py
@@ -7,7 +7,7 @@ def create_synthData_new(v=2, N=400, outDir='./', device='cpu', mode=1, F=20):
     N : number of data
     F$ : number of features in view $ 
     '''
-    #np.random.seed(0)
+    #np.random.seed(1)
     #torch.manual_seed(0)
 
     random_seeds = np.random.randint(0, 2**16 - 1, size=3)